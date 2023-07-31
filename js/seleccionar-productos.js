e183c70cc7b7b0be352e4b042e1c6c1fe824358f
unknown <gercameto3@gmail.com>
2023-07-31 02:15:20 -0300

    correcionesjs

--- js/seleccionar-productos.js
+++ js/seleccionar-productos.js
@@ -35,7 +35,7 @@ function obtenerProductosSeleccionados(productosDisponibles) {
 		totalCarrito = carrito.reduce((acc,prod) => acc + (prod.precioUnitario * prod.cantidad),0)
 		console.log(totalCarrito);
 
-		const agregarOtraBebida = prompt("¿Deseas agregar otro artículo? (S/N)").toLowerCase();
+		const agregarOtroArticulo = prompt("¿Deseas agregar otro artículo? (S/N)").toLowerCase();
 		seguirAgregando = agregarOtroArticulo === "S";
 	}
 	console.table(carrito);
